55953a4a6360be0810d358e460bb6fe5335bfbf8
Aparna choudhury <choudhuryaparna11@gmail.com>
2026-01-20 12:52:34 +0530

    Updated project features

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const{PORT} = require('./config');
+const apiroutes = require('./routes');
 const app=express();
+app.use('/api',apiroutes);
 app.listen(PORT,()=>{
     console.log(`successfully started the server on PORT :${PORT}`);
 
